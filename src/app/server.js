339a88291507a9b7d70ea58266453797d83fa842
Stanley Ogbonna <ogbonnastanley@gmail.com>
2020-01-07 15:41:50 +0100

    initial commit deploy

--- src/app/server.js
+++ src/app/server.js
@@ -1,16 +1,9 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
+const path = require("path");
+const express = require("express");
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.use(express.static(__dirname + '/angular-build'));
+app.get('/*', function(req,res){
+res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
